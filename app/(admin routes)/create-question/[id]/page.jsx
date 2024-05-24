3c14ebb078f8eec00b293cb2a9510ba8d8d729de
lakshya104 <lakshya.vyas@anktech.co.in>
2024-05-24 17:13:19 +0530

    hello

--- app/(admin routes)/create-question/[id]/page.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { QuestionForm } from "./question-form";
-
-const CreateQuestion = ({ params }) => {
-  return <QuestionForm id={params.id} />;
-};
-
-export default CreateQuestion;
